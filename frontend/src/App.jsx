57a7041151839f19d8f26ac44bc4fc984ef547d1
abkux <kumarjhaabhishek2002@gmail.com>
2025-02-20 23:45:25 +0530

    fix in the backend url

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 
-const API_URL = "https://optiprods-intern.onrender.com/"; // Backend Deployment Link
+const API_URL = "https://optiprods-intern.onrender.com/tasks"; // Backend Deployment Link
 
 function App() {
     const [tasks, setTasks] = useState([]);
